2067baea5e451ab48e52daf19c1359df7d3a4234
datdatqt <Justinepasion2@gmail.com>
2023-09-30 14:36:31 +0800

    menu update
    sushi

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,31 @@
-let list = document.querySelectorAll(".list li");
-let box = document.querySelectorAll(".box");
+let list1 = document.querySelectorAll(".title-1 li");
+let box1 = document.querySelectorAll(".menu-1");
 
-list.forEach((el)=>{
+list1.forEach((el)=>{
     el.addEventListener("click" , (e)=>{
-        list.forEach((el1)=>{
+        list1.forEach((el1)=>{
             el1.style.color = "#fff";
         })
         e.target.style.color = "#A61008"
-        box.forEach((el2)=>{
+        box1.forEach((el2)=>{
+            el2.style.display = "none";
+        })
+        document.querySelectorAll(e.target.dataset.color).forEach((el3)=>{
+            el3.style.display = "flex";
+        })
+    })
+})
+
+let list2 = document.querySelectorAll(".list2 li");
+let box2 = document.querySelectorAll(".box-2");
+
+list2.forEach((el)=>{
+    el.addEventListener("click" , (e)=>{
+        list2.forEach((el1)=>{
+            el1.style.color = "#fff";
+        })
+        e.target.style.color = "#A61008"
+        box2.forEach((el2)=>{
             el2.style.display = "none";
         })
         document.querySelectorAll(e.target.dataset.color).forEach((el3)=>{
